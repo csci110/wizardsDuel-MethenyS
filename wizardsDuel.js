5352a9550ac408f8c99bc088bb9ed0f1e05583f7
Savannah Metheny <methenys@dewv.edu>
2018-09-17 17:03:07 +0000

    Updates via sync script

--- wizardsDuel.js
+++ wizardsDuel.js
@@ -1,80 +1,147 @@
-import {game, Sprite} from "./sgc/sgc.js";
+import { game, Sprite } from "./sgc/sgc.js";
 
-game.setBackground ("floor.png");
+game.setBackground("floor.png");
 
 
 class playerWizard extends Sprite {
-    constructor(){
+    constructor() {
         super();
         this.name = "Marcus the Wizard";
         this.width = 48;
         this.height = 48;
-        this.setImage ("marcusSheet.png");
+        this.setImage("marcusSheet.png");
         this.x = this.width;
-        this.y = this.y;
+        this.y = this.height;
         this.defineAnimation("down", 6, 8);
         this.defineAnimation("up", 0, 2);
         this.defineAnimation("right", 3, 5);
         this.defineAnimation("left", 9, 11);
         this.speedWhenWalking = 100;
-      
+
     }
-    handleDownArrowKey(){
+    handleDownArrowKey() {
         this.playAnimation("down");
         this.speed = this.speedWhenWalking;
         this.angle = 270;
     }
-    handleUpArrowKey(){
+    handleUpArrowKey() {
         this.playAnimation("up");
         this.speed = this.speedWhenWalking;
         this.angle = 90;
     }
-    handleRightArrowKey(){
-        this.playAnimation("right");
-        this.speed = this.speedWhenWalking;
-        this.angle = 360;
-    }
-    handleLeftArrowKey(){
-        this.playAnimation("left");
-        this.speed = this.speedWhenWalking;
-        this.angle = 180;
-    }
-
-       handleGameLoop(){
-        this.y = Math.max(5, this.y);     //Picks the greatest of 0, or the current value of y and assigns it to y
-        this.y = Math.min(game.displayHeight - this.height, this.y);  //Keeps Marcus in the display window
+    
+  handleGameLoop() {
+        this.y = Math.max(5, this.y); //Picks the greatest of 0, or the current value of y and assigns it to y
+        this.y = Math.min(game.displayHeight - this.height, this.y); //Keeps Marcus in the display window
+        this.x = Math.max(0, this.x);
+        this.x = Math.min(game.displayWidth - this.width , this.x);
         this.speed = 0;
     }
+    
+    handleSpacebar() {
+        let spell = new Spell();
+        spell.name = "A spell from Marcus";
+        spell.setImage("marcusSpellSheet.png");
+        spell.x = this.x + this.width;
+        spell.y = this.y;
+        spell.angle = 0;
+        this.playAnimation("right");
+    }
+
+  
 
 }
 
 let marcus = new playerWizard();
 marcus.name = "Marcus the Wizard";
-marcus.width = 48;
-marcus.height = 48;
 marcus.setImage("marcusSheet.png");
-marcus.x = this.width;
-marcus.y = this.y;
 
-class Spell extends Sprite{
-    constructor(){
+class Spell extends Sprite {
+    constructor() {
+        super();
         this.speed = 200;
         this.height = 48;
         this.width = 48;
         this.defineAnimation("magic", 0, 7);
         this.playAnimation("magic", true);
     }
+    handleBoundaryContact() {
+        //Delete spell when it leaves the display area
+        game.removeSprite(this);
+    }
+
+    handleCollision(otherSprite) {
+        if (this.getImage() !== otherSprite.getImage()) {
+            let verticaloffset = Math.abs(this.y - otherSprite.y);
+            if (verticaloffset < this.height / 2) {
+                game.removeSprite(this);
+                new Fireball(otherSprite);
+            }
+        }
+        return false;
+    }
+}
+
+class NonPlayerWizard extends Sprite {
+    constructor() {
+        super();
+        this.name = "The Mysterious Stranger";
+        this.setImage("strangerSheet.png");
+        this.width = 48;
+        this.height = 48;
+        this.x = game.displayWidth - 2 * this.width;
+        this.y = this.height;
+        this.angle = 270;
+        this.speed = 150;
+        this.defineAnimation("up", 0, 2);
+        this.defineAnimation("down", 6, 8);
+        this.defineAnimation("left", 9, 11);
+        this.playAnimation("down", true);
+    }
+    handleGameLoop() {
+        if (this.y <= 0) {
+            // Upward motion has reached top, so turn down
+            this.y = 0;
+            this.angle = 270;
+            this.playAnimation("down", true);
+        }
+        if (this.y >= game.displayHeight - this.height) {
+            // Downward motion has reached bottom, so turn up
+            this.y = game.displayHeight - this.height;
+            this.angle = 90;
+            this.playAnimation("up", true);
+        }
+        let spell = new Spell();
+        spell.name = "A spell cast by the Stranger";
+        spell.setImage("strangerSpellSheet.png");
+        spell.x = this.x + this.width;
+        spell.y = this.y;
+        spell.angle = 180;
+        this.playAnimation("left");
+    }
+    handleAnimationEnd() {
+        if (this.angle === 90) {
+            this.playAnimation("up");
+        }
+        if (this.angle === 270) {
+            this.playAnimation("down");
+        }
+    }
 
 }
 
-class spell extends Spell{
-    constructor(){
-        this.name = "A spell cast by Marcus";
-        this.setImage("marcusSpellSheet.png");
-        this.angle = 0;
+let stranger = new NonPlayerWizard();
+
+class Fireball extends Sprite() {
+    constructor(deadSprite) {
+        super();
+        this.name = "A ball of fire";
+        this.x = deadSprite.x;
+        this.y = deadSprite.y;
+        this.setImage("fireballSheet.png");
+        game.removeSprite(deadSprite);
+        this.defineAnimation("explode", 0, 16);
+        this.playAnimation("explode");
     }
-    handleSpaceBar(){
-        this.angle = 0;
-        this.playAnimation("magic");
-    }
+
 }
